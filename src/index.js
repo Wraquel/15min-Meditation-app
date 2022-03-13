8ee098c9b65c9534ac688c940867cb7d8b20e05e
Wraquel <89659478+Wraquel@users.noreply.github.com>
2022-03-13 16:09:30 +0000

    Javascript improve

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ var start = document.getElementById("start");
 var reset = document.getElementById("reset");
 
 //declare variables
-var minutes = document.getElementById("minutes");
-var seconds = document.getElementById("seconds");
+var wMinutes = document.getElementById("minutes");
+var wSeconds = document.getElementById("seconds");
 
 var bell_meditate = new Audio("./midia/bell_meditate.mp3");
 
 //declare a reference to a countner variable that can be undefide or define in functions
 var startCountner;
+
 // Começar o timer-estabelecer intervalo de 1000 ms, quando a variavel startCountner for indefinida
 function Start() {
   bell_meditate.play();
@@ -23,40 +24,50 @@ function Start() {
       .classList.add("meditation-title");
     document.getElementById("pulse").style.animation =
       "pulse 4s infinite alternate";
-  } else alert("Time is already running");
+  } else
+    alert(
+      "Time is already running or please Reset the timer for another cycle."
+    );
 }
-
 // Resetar o timer-parar intervalo, quando a variavel startCountner for indefinida e reestabelecer timer inicial
 function Reset() {
-  minutes.innerText = 15;
-  seconds.innerText = "00";
+  wMinutes.innerText = 15;
+  wSeconds.innerText = "00";
   stopInterval();
   startCountner = undefined;
 }
-
+// Se segundos forem menores que 10 colocar 0 antes da variável
+function formatTimer() {
+  if (wSeconds.innerText <= 9) {
+    wSeconds.innerText = `0${wSeconds.innerText}`;
+  }
+}
 //Start Timer quando os segundos forem diferentes de 0, diminuir os segundos de 1 em 1.
 //Mas se os minutos forem diferentes de 0 e os segundosiguais a 0, tornar segundos 59
 //e diminuir minutos de 1 em 1
 function timer() {
-  if (seconds.innerText != 0) {
-    seconds.innerText--;
-  } else if (minutes.innerText != 0 && seconds.innerText == 0) {
-    seconds.innerText = 59;
-    minutes.innerText--;
-  } else if (seconds.innerText == 0 && minutes.innerText == 0) {
+  if (wSeconds.innerText != 0) {
+    wSeconds.innerText--;
+    formatTimer();
+  } else if (wMinutes.innerText != 0 && wSeconds.innerText == 0) {
+    wSeconds.innerText = 59;
+    wMinutes.innerText--;
+    // Se minutos forem menores que 10 colocar 0 antes da variável
+    if (wMinutes.innerText <= 9) {
+      wMinutes.innerText = `0${wMinutes.innerText}`;
+    }
+  } else if (wSeconds.innerText == 0 && wMinutes.innerText == 0) {
     document.getElementById("daily-title").innerHTML = "";
     document.getElementById("meditation-title").innerHTML = "Namastê";
     document
       .getElementById("meditation-title")
       .classList.add("message-namaste");
     audio();
-    Reset();
+    stopInterval();
   }
 }
 function audio() {
   bell_meditate.play();
-
-  Reset();
 }
 // Parar o timer-limpar intervalo
 function stopInterval() {
